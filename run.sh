4844d77291bafd44c9af0d3fa4ea33a98f660389
Carine Dengler <carine-dengler@zo.uni-heidelberg.de>
2018-09-27 10:30:55 +0200

    run.sh
    - stripped down to Ubuntu
    - removed sudo (instead drop privileges)
    - removed adding docker group (should be done when installing docker)

--- run.sh
+++ run.sh
@@ -1,52 +1,36 @@
 #!/bin/bash
 
-ansible --version
-
-# Install Ansible
-if [ $? -ne 0 ]; then
-    sudo apt-get update
-
-    if [ $? -eq 0 ]; then
-        sudo apt-get install -qqy software-properties-common
-        sudo apt-add-repository ppa:ansible/ansible -y
-        sudo apt-get update
-        sudo apt-get install -qqy ansible
-    else
-        sudo yum install -y epel-release
-        sudo yum install -y ansible
-
-	if [ $? -ne 0 ]; then
-	    sudo pip install ansible
-        fi
-
-        # allow nginx permission to access localhost docker
-        setsebool httpd_can_network_connect on -P
-    fi
+if [$SUDO_USER];
+then
+	CURR_USER = $SUDO_USER
+else
+	CURR_USER = $(whoami)
 fi
 
-# Install Remote Roles
-if ! sudo ansible-galaxy list | grep -q 'nginx'; then
-    sudo ansible-galaxy install -r requirements.yml
-    sudo chown -R $USER ~/.ansible
+
+VERSION = $(sudo -u $CURR_USER ansible --version)
+
+
+if [$? -ne 0];
+then
+	echo "Package ansible is not installed. Installing ..."
+	apt-get update
+	apt-get install software-properties-common
+	apt-add-repository ppa:ansible/ansible
+	apt-get update
+	apt-get install ansible
 fi
 
-group=docker
-cmd="ansible-playbook -i hosts ./playbook.yml"
+echo "Enabling httpd_can_network_connect ..."
+sudo -u $CURR_USER setsebool -P httpd_can_network_connect on
 
-# If not in 'docker' group, need to switch to 
-# group to allow to docker ops as non-root user
-if [ $(id -gn) != $group ]; then
-  if ! grep -q $group /etc/group; then
-    sudo groupadd docker
-  fi
+NGINX = $(ansible-galaxy list | sudo -u $CURR_USER grep nginx)
 
-  if ! groups | grep -q $group; then
-    sudo usermod -aG docker $USER
-    exec sg $group "$cmd --extra-vars \"$1\""
-    return
-  fi
+if [$? -ne 0];
+then
+	"Installing roles from Galaxy ..."
+	ansible-galaxy install -r requirements.yml
+	chown -R $CURR_USER
 fi
 
-echo $cmd
-$cmd --extra-vars "$1"
-
+sudo -u ansible-playbook -i hosts playbook.yml --extra_vars $1
